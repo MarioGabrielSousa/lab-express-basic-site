270f661d471c773619aa26244a7146034e009d64
mario sousa <vegetalizado@gmail.com>
2021-04-14 23:17:01 +0100

    Done

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/intro", (request, response) => {
+  response.sendFile(__dirname + "/views/intro.html");
+});
+
 app.get("/home", (request, response) => {
   response.sendFile(__dirname + "/views/home.html");
 });
 
 app.get("/about", (request, response) => {
   response.sendFile(__dirname + "/views/about.html");
-});
-
-app.get("/portfolio", (request, response) => {
-  response.sendFile(__dirname + "/views/portfolio.html");
 });
 
 app.get("/photos", (request, response) => {
@@ -22,7 +22,6 @@ app.get("/photos", (request, response) => {
 app.get("/itoldyou", (request, response) => {
   response.sendFile(__dirname + "/views/itoldyou.html");
 });
-
 
 app.listen(3000, () => {
   console.log("My app is listening on port 3000");
